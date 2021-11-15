b906d5322e7f5ed11f5d69d40abc3cb0dd7e558a
MohTaher108 <hamodetaher@gmail.com>
2021-11-15 17:01:45 -0500

    Clear list method done

--- src/main/java/ucf/assignments/Controller.java
+++ src/main/java/ucf/assignments/Controller.java
@@ -5,23 +5,29 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Controller {
-    @FXML
-    private TableView<Item> tableView;
+    // My main tableview that will contain everything
+    @FXML private TableView<Item> tableView;
+    @FXML private TableColumn<Item, String> NameColumn;
+    @FXML private TableColumn<Item, TextArea> DescriptionColumn;
+    @FXML private TableColumn<Item, LocalDate> DueDateColumn;
+    @FXML private TableColumn<Item, CheckBox> MarkCompleteColumn;
+    @FXML private TableColumn<Item, CheckBox> ContextMenuColumn;
 
-    @FXML
-    private TableColumn<Item, String> NameColumn;
+    // Extra tableview that will show when the user needs it
+    @FXML private TableView<Item> tableViewExtra;
+    @FXML private TableColumn<Item, String> NameColumnExtra;
+    @FXML private TableColumn<Item, TextArea> DescriptionColumnExtra;
+    @FXML private TableColumn<Item, LocalDate> DueDateColumnExtra;
+    @FXML private TableColumn<Item, CheckBox> MarkCompleteColumnExtra;
+    @FXML private TableColumn<Item, CheckBox> ContextMenuColumnExtra;
 
-    @FXML
-    private TableColumn<Item, TextArea> DescriptionColumn;
+    // ArrayList that will hold all my items
+    ArrayList<Item> ItemList = new ArrayList<>();
 
-    @FXML
-    private TableColumn<Item, LocalDate> DueDateColumn;
-
-    @FXML
-    private TableColumn<Item, CheckBox> MarkCompleteColumn;
-
+    // Initialize all my columns
     @FXML
     public void initialize() {
         // Set up the columns in the table
@@ -29,51 +35,56 @@ public class Controller {
         DescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("Description"));
         DueDateColumn.setCellValueFactory(new PropertyValueFactory<>("DueDate"));
         MarkCompleteColumn.setCellValueFactory(new PropertyValueFactory<>("MarkComplete"));
+        ContextMenuColumn.setCellValueFactory(new PropertyValueFactory<>(""));
+
+        // Set up the extra columns in the extra table
+        NameColumnExtra.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        DescriptionColumnExtra.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        DueDateColumnExtra.setCellValueFactory(new PropertyValueFactory<>("DueDate"));
+        MarkCompleteColumnExtra.setCellValueFactory(new PropertyValueFactory<>("MarkComplete"));
+        ContextMenuColumnExtra.setCellValueFactory(new PropertyValueFactory<>(""));
     }
 
+    // This method will add an item when the MenuItem is clicked
     @FXML
     protected void AddItem() {
-        Item item = new Item("Item", LocalDate.now());
-        tableView.getItems().add(item);
-        /*
-        String Name = "Item 1";
-        String Description = "Desc";
-        LocalDate DueDate = LocalDate.now();
-        tableView.getItems().add(new Person(Name, Description, DueDate));
-         */
+        /*  ItemList.add(item)
+            tableView.add(item) */
+        // Add the new item to our arrayList
+        ItemList.add(new Item("Item", LocalDate.now()));
+        // Add the new item into the TableView
+        tableView.getItems().add(ItemList.get(ItemList.size()-1));
     }
 
+    // This method will clear my TableView (Todo List)
     @FXML
-    protected void editListTitle() {
-        /*
-        EditNameOfTab(string)
-         */
+    protected void ClearList() {
+        tableView.getItems().clear();
     }
 
     @FXML
     protected void DisplayAllItemsInList() {
-        /*
-        for(all titledPanes)
-            display titledPane
-         */
+        tableViewExtra.getItems().clear();
+        for(Item item: ItemList)
+            tableViewExtra.getItems().add(item);
     }
 
     @FXML
     protected void DisplayCompleteItemsInList() {
-        /*
-        for(all titledPanes)
-            if(Tab.AnchorPane.Accordion.TitledPane complete?)
-                display titledPane
-         */
+        tableViewExtra.getItems().clear();
+        for(Item item: ItemList) {
+            if(!item.getMarkComplete().isSelected())
+                tableViewExtra.getItems().add(item);
+        }
     }
 
     @FXML
     protected void DisplayIncompleteItemsInList() {
-        /*
-        for(all titledPanes)
-            if(!Tab.AnchorPane.Accordion.TitledPane complete?)
-                display titledPane
-         */
+        tableViewExtra.getItems().clear();
+        for(Item item: ItemList) {
+            if(!item.getMarkComplete().isSelected())
+                tableViewExtra.getItems().add(item);
+        }
     }
 
     @FXML
--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -1,43 +1,29 @@
 package ucf.assignments;
 
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 import java.time.LocalDate;
 
-/*
-// Class for the context menu of every item
-class ItemContextMenu {
-    // Make context menu, and menu items for removing items and marking items complete
-    ContextMenu menu = new ContextMenu();
-    MenuItem removeItem = new MenuItem("Remove Item");
-    MenuItem markComplete = new MenuItem("Mark Complete");
-
-    // Constructor that adds all our menu items to the context menu (since we always need that)
-    public ItemContextMenu() {
-        menu.getItems().addAll(removeItem, markComplete);
-    }
-}
-*/
-
 // Class for each item we will implement
 public class Item {
+    // Class's attributes and contextMenu
     private String Name;
     private TextArea Description;
     private LocalDate DueDate;
     private CheckBox MarkComplete;
+    ItemContextMenu itemMenu;
 
+    // Construct item's stuff
     public Item(String Name, LocalDate DueDate) {
         this.Name = Name;
         this.Description = new TextArea("Description");
         this.Description.setWrapText(true);
         this.DueDate = DueDate;
         this.MarkComplete = new CheckBox();
-        // ItemContextMenu itemMenu = new ItemContextMenu();
+        this.itemMenu = new ItemContextMenu();
     }
 
+    // All my getters and setters
     public String getName() {
         return Name;
     }
@@ -50,8 +36,8 @@ public class Item {
         return Description;
     }
 
-    public void setDescription(TextArea Description) {
-        this.Description = Description;
+    public void setDescription(String Description) {
+        this.Description.setText(Description);
     }
 
     public LocalDate getDueDate() {
@@ -66,7 +52,20 @@ public class Item {
         return MarkComplete;
     }
 
-    public void setMarkComplete(CheckBox markComplete) {
+    public void setMarkComplete(CheckBox MarkComplete) {
         this.MarkComplete = MarkComplete;
+    }
+}
+
+// Class for the context menu of every item
+class ItemContextMenu extends ContextMenu {
+    // Make context menu, and menu items for removing items and marking items complete
+    ContextMenu menu = new ContextMenu();
+    MenuItem removeItem = new MenuItem("Remove Item");
+    MenuItem markComplete = new MenuItem("Mark Complete");
+
+    // Constructor that adds all our menu items to the context menu (since we always need that)
+    public ItemContextMenu() {
+        menu.getItems().addAll(removeItem, markComplete);
     }
 }
